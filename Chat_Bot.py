b266ad5fb3893c4734b3a4354f026060924669c4
Sasen Perera <91795267+Sas2k@users.noreply.github.com>
2021-10-12 18:37:57 +0530

    Update Chat_Bot.py

--- Chat_Bot.py
+++ Chat_Bot.py
@@ -121,4 +121,4 @@ async def on_message(message):
             await message.channel.send(f'I Won i chose {computer_in}')
         await message.channel.send(f"player: {user_in}, computer: {computer_in}")
 
-client.run('ODk3MDQ1ODU4ODIwOTUyMDk0.YWP9Dw.uXzOYMt8sv4LdbSYD-EFybzOMDA')
+client.run(#ADD YOUR BOT TOKEN HERE#)
